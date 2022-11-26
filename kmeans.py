45605408d195f2c2ce7025e2c6575a601f464f18
timothymclark0 <timothymclark0@gmail.com>
2022-11-26 09:12:06 -0500

    projectmidpoint

--- kmeans.py
+++ kmeans.py
@@ -1,21 +1,7 @@
 #hi
-import numpy as np
-import pandas as pd
-import sklearn
-import matplotlib.pyplot as plt
-import seaborn as sns 
 
+import re
 
-
-dataset = pd.read_csv('Mall_Customers.csv')
-print(dataset.head())
-print(dataset.shape)
-#sns.distplot(dataset['Annual Income (k$)'],kde=False,bins=50)
-#sns.regplot(x='Annual Income (k$)', y="Spending Score (1-100)", data=dataset)
-#plt.show()
-#input()
-
-hival = dataset.filter(["Annual Income (k$)", "Spending Score (1-100)"],axis = 1)
-print(hival.head())
-
-km_model = KMeans(n_clusters=4)
+strings = "howdyhowdy\nhowdy"
+string2 = re.sub('(.)',r'\1  ',strings)
+print(string2)
